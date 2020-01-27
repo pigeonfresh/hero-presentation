ce19f1097da8536c92c78c95d8231d18522c5778
eelke <eelkevanderbij.prive+github@gmail.com>
2020-01-27 17:02:56 -0300

    Add muban transition component and build template

--- build-tools/generator-template/block/{name_sc}/{name_pc}.ts
+++ build-tools/generator-template/block/{name_sc}/{name_pc}.ts
@@ -1,13 +1,12 @@
-import AbstractBlock from '../AbstractBlock';
+import AbstractTransitionBlock from 'app/component/block/AbstractTransitionBlock';
+import {{name_pc}}TransitionController from './{{name_pc}}TransitionController';
 
-export default class {{name_pc}} extends AbstractBlock {
-  public static readonly displayName:string = '{{name_sc}}';
+export default class {{name_pc}} extends AbstractTransitionBlock {
+  public static displayName:string = '{{name_sc}}';
+  public transitionController:{{name_pc}}TransitionController;
 
   constructor(el:HTMLElement) {
     super(el);
-  }
-
-  public dispose() {
-    super.dispose();
+    this.transitionController = new {{name_pc}}TransitionController(this);
   }
 }
---
+++ build-tools/generator-template/block/{name_sc}/{name_pc}TransitionController.ts
@@ -0,0 +1,13 @@
+import { MubanTransitionController, IMubanTransitionMixin } from 'muban-transition-component';
+import { TimelineMax } from 'gsap';
+import {{name_pc}} from './{{name_pc}}';
+
+class {{name_pc}}TransitionController extends MubanTransitionController {
+  protected setupTransitionInTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {}
+
+  protected setupTransitionOutTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {}
+
+  protected setupLoopingAnimationTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {}
+}
+
+export default {{name_pc}}TransitionController;
