5e6ebca3d419a61d486fa9440f622b44aa47ea64
eelke <eelkevanderbij.prive+github@gmail.com>
2020-01-27 17:40:31 -0300

    Update generator template

--- build-tools/generator-template/block/{name_sc}/{name_pc}TransitionController.ts
+++ build-tools/generator-template/block/{name_sc}/{name_pc}TransitionController.ts
@@ -1,13 +1,17 @@
 import { MubanTransitionController, IMubanTransitionMixin } from 'muban-transition-component';
 import { TimelineMax } from 'gsap';
 import {{name_pc}} from './{{name_pc}}';
+// import { verticalFadeIn } from '../../../util/transition-helpers';
 
 class {{name_pc}}TransitionController extends MubanTransitionController {
-  protected setupTransitionInTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {}
+protected setupTransitionInTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {
+    // const transitionItems = parent.getElements('[data-transition-item]');
+    // timeline.add(verticalFadeIn(transitionItems))
+  }
 
-  protected setupTransitionOutTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {}
+protected setupTransitionOutTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {}
 
-  protected setupLoopingAnimationTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {}
+protected setupLoopingAnimationTimeline(timeline:TimelineMax, parent:{{name_pc}}, id:string): void {}
 }
 
 export default {{name_pc}}TransitionController;
